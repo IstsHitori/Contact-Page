73e3d1019c92e5e050a02fde60bf986bdd69212e
Franco <fjavierserrano@unicesar.edu.co>
2023-10-09 12:13:15 -0500

    tercer commit

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
-
+let lista;
+let li;
 function mostrarLista(id){
-    const lista = document.querySelector(id);
+    lista = document.querySelector(id);
+
     let clases = lista.classList;
-    if(clases.contains("active")){
-        clases.remove("active");
-    }else{
-        clases.add("active");
-    }
+    clases.toggle("active");
 }
 
+function seleccionarOpcion(id){
+
+    li = document.querySelector(id);
+    console.log(li.textContent);
+
+    if(li.id === "li-1-1" || li.id === "li-1-2" || li.id === "li-1-3"){
+        let span_text = document.querySelector("#span-text-1");
+        span_text.textContent = li.textContent;
+        lista.classList.remove("active");   
+    }else if(li.id === "li-2-1" || li.id === "li-2-2" || li.id === "li-2-3"){
+        let span_text = document.querySelector("#span-text-2");
+        span_text.textContent = li.textContent;
+        lista.classList.remove("active");   
+    }
+}
